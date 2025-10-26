945934d1f9160ce4cb2fa1c1f1446fb6f83bd49a
gabidevx <gabicalinciuc66@gmail.com>
2025-10-26 15:55:47 +0200

    flex

--- src/app/(root)/(routes)/traineri/page.tsx
+++ src/app/(root)/(routes)/traineri/page.tsx
@@ -4,15 +4,21 @@ import type { FC } from "react";
 
 const TrainersPage: FC = async () => {
   return (
-    <div>
-      <Card>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-50">
+      <Card className="w-80 shadow-lg">
         <CardHeader>
-          <CardTitle>{/* Title me pls */}</CardTitle>
-          <CardContent>
-            <Image />
-            {/* no hentai pls */}
-          </CardContent>
+          <CardTitle className="text-center">Trainer Profile</CardTitle>
         </CardHeader>
+        <CardContent className="flex flex-col items-center justify-center">
+          <Image
+            src="/images/ash.png"
+            alt="Ash Ketchum"
+            width={150}
+            height={150}
+            className="rounded-full object-cover mb-4"
+          />
+          <p className="text-center text-gray-700">Ash Ketchum from Pallet Town</p>
+        </CardContent>
       </Card>
     </div>
   );
